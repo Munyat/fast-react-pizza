9a3c1d155c809322fd787b306dd7158992942d43
Brian Kipkirui Cheruiyot <briankipkiruimunyat@gmail.com>
2025-03-11 11:08:23 +0300

    styled using tailwind css

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,11 +1,15 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
+import User from "../features/user/Username";
 
 function Header() {
   return (
-    <header>
-      <Link to="/">Fast React Pizza Co</Link>
+    <header className="order-b-2 flex h-16 items-baseline justify-between border-stone-200 bg-yellow-500 px-4 py-3 align-baseline uppercase sm:px-6">
+      <Link to="/" className="tracking-widest">
+        Fast React Pizza Co.
+      </Link>
       <SearchOrder />
+      <User />
     </header>
   );
 }
